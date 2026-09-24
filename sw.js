d0f1eb5cb02c3c16995f02974191702228472159
gkoka892517-ui <gkoka892517@gmail.com>
2026-09-24 19:15:54 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const V="finance-v1";
+const FILES=["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==V).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(fetch(e.request).then(res=>{const c=res.clone();caches.open(V).then(ch=>ch.put(e.request,c));return res}).catch(()=>caches.match(e.request,{ignoreSearch:true})));
+});
