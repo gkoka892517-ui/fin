ac63fbc04216c6b323e755a4aa06ff37ff3b2721
gkoka892517-ui <gkoka892517@gmail.com>
2026-09-25 03:24:07 +0300

    Update service worker cache version to finance-v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V="finance-v1";
+const V="finance-v3";
 const FILES=["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==V).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
